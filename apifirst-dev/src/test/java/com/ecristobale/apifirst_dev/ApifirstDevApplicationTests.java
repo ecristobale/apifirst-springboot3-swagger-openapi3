d81bf78257f31657947648e4f9cf7847d8388d71
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-22 21:44:19 +0100

    created with spring initializr

---
+++ apifirst-dev/src/test/java/com/ecristobale/apifirst_dev/ApifirstDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecristobale.apifirst_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
