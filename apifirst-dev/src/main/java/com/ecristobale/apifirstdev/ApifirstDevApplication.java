e937605a43777e4aa9a74e34f754c6adb6c15b1d
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-22 22:54:48 +0100

    changed package name

---
+++ apifirst-dev/src/main/java/com/ecristobale/apifirstdev/ApifirstDevApplication.java
@@ -0,0 +1,13 @@
+package com.ecristobale.apifirstdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifirstDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifirstDevApplication.class, args);
+	}
+
+}
---
+++ apifirst-dev/src/test/java/com/ecristobale/apifirstdev/ApifirstDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecristobale.apifirstdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifirstDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
