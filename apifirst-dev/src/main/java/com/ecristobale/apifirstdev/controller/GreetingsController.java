d70308f5bb0a33aaf1c9bf26373beeecf452c205
CEED\Ceed <eduardo.cristobalenriquez0388@gmail.com>
2025-03-22 23:32:51 +0100

    Expose generated api with rest controller for GET request: /greetings

--- apifirst-dev/src/main/java/com/ecristobale/apifirstdev/controller/GreetingsController.java
+++ apifirst-dev/src/main/java/com/ecristobale/apifirstdev/controller/GreetingsController.java
@@ -1,5 +1,19 @@
 package com.ecristobale.apifirstdev.controller;
 
 
-public class GreetingsController {
+import com.ecristobale.apifirstdev.api.GreetingsApi;
+import com.ecristobale.apifirstdev.model.Greetings;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController implements GreetingsApi {
+
+    @Override
+    public ResponseEntity<Greetings> getGreetings() {
+        String hi = "Hello, world!";
+        Greetings greetings = new Greetings();
+        greetings.setHi(hi);
+        return ResponseEntity.ok(greetings);
+    }
 }
